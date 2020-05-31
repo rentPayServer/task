4eb7df68ad3dbbf17949caf609c3a1799df13a59
Tc <jackgoodchen2019@gmail.com>
2020-06-01 00:16:24 +0800

    upd

--- config/__init__.py
+++ config/__init__.py
@@ -14,7 +14,7 @@ common['images'] = os.path.join(common['static'],"images")
 
 mysql=dict(
 	host = os.environ.get('DBHOST', 'localhost'),
-	port = os.environ.get('DBPORT', 3306),
+	port = int(os.environ.get('DBPORT', 3306)),
 	user = os.environ.get('DBUSER', 'root'),
     name = os.environ.get('DBNAME', 'allwin_new'),
 	password = os.environ.get('DBPASS', '123456'),
